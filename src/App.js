b73c7a9723ad857893d7ee9aca450cdaff9511a7
Michał Klepner <michal.klepner1@o2.pl>
2024-09-13 10:24:22 +0200

    starter files for project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Header from "./Header";
+import { useReducer } from "react";
+function reducer(type, action) {}
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+       
+      </main>
+    </div>
+  );
+}
+
+export default App;
